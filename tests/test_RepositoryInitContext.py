3956b93459ed5a1ae6e5ede2b14568603f16e344
Piv <codemonster123@gmail.com>
2025-05-10 09:33:34 -0500

    Added test_RepositoryInitContext.py

---
+++ tests/test_RepositoryInitContext.py
@@ -0,0 +1,34 @@
+from sys import path
+from pathlib import Path; 
+path.append(str(Path(__file__).parent.parent)+'/src/')
+
+from smtp.IncidentRepository import IncidentRepository
+from smtp.Incident import Incident
+from smtp.RepositoryInitContext import RepositoryInitContext
+import pytest
+from unittest.mock import patch
+from pprint import pprint
+
+def test_server_invalid():
+    # Make sure server parameter is not missing
+    with pytest.raises(ValueError) as ex_info:
+        RepositoryInitContext(server='',database='database',username='username',password='password')
+    assert 'Server is missing' in ex_info.value.args[0]
+
+def test_database_invalid():
+    # Make sure server parameter is not missing
+    with pytest.raises(ValueError) as ex_info:
+        RepositoryInitContext(server='server',database='',username='username',password='password')
+    assert 'Database is missing' in ex_info.value.args[0]
+
+def test_username_invalid():
+    # Make sure server parameter is not missing
+    with pytest.raises(ValueError) as ex_info:
+        RepositoryInitContext(server='server',database='database',username='',password='password')
+    assert 'Username is missing' in ex_info.value.args[0]
+
+def test_password_invalid():
+    # Make sure server parameter is not missing
+    with pytest.raises(ValueError) as ex_info:
+        RepositoryInitContext(server='server',database='database',username='username',password='')
+    assert 'Password is missing' in ex_info.value.args[0]
